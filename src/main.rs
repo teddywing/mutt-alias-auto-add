3242b7d4cc1dbc785804fa784151c2ba74da1902
Teddy Wing <git@teddywing.com>
2016-04-05 20:58:30 -0400

    Add a comment with some implementation details
    Clarify what we want to do.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+// if alias not new
+// 	do nothing
+// if alias is new
+// 	if real_alias is in alias file
+// 		if email address is in alias file
+// 			do nothing
+// 		
+// 		find all instances of real_alias
+// 			get instance with greatest id number
+// 			
+// 			change real_alias to "#{real_alias}-#{id + 1}"
+// 			
+// 			if only one instance
+// 				append "-2" to real_alias
+// 	
+// 	add alias to alias file
 }
