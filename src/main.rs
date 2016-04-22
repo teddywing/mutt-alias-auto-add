622dbe1cf22472c4b72867a7d34b289afaba5160
Teddy Wing <git@teddywing.com>
2016-04-22 08:40:46 -0400

    main: Remove pseudocode
    The program is nearly implemented and this information is no longer
    needed.

--- src/main.rs
+++ src/main.rs
@@ -141,21 +141,4 @@ fn main() {
     for l in &input {
         println!("{}", l);
     }
-
-// if alias not new
-// 	do nothing
-// if alias is new
-// 	if real_alias is in alias file
-// 		if email address is in alias file
-// 			do nothing
-// 		
-// 		find all instances of real_alias
-// 			get instance with greatest id number
-// 			
-// 			change real_alias to "#{real_alias}-#{id + 1}"
-// 			
-// 			if only one instance
-// 				append "-2" to real_alias
-// 	
-// 	add alias to alias file
 }
